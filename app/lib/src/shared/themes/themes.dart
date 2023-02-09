8f44e7a4e876ef56611413c1f2d4bcb207635ed2
MarckVinny <mvrpj@msn.com>
2023-02-09 19:20:45 -0300

    transformando o ThemeData em um getter

--- app/lib/src/shared/themes/themes.dart
+++ app/lib/src/shared/themes/themes.dart
@@ -1,29 +1,34 @@
 part of '../../../main.dart';
 
-final lightTheme = ThemeData(
-  useMaterial3: true,
-  colorScheme: _lightColorScheme,
-  appBarTheme: AppBarTheme(
-    centerTitle: true,
-    backgroundColor: _lightColorScheme.primaryContainer,
-    foregroundColor: _lightColorScheme.onPrimaryContainer,
-  ),
-  floatingActionButtonTheme: FloatingActionButtonThemeData(
-    backgroundColor: _lightColorScheme.primary,
-    foregroundColor: _lightColorScheme.onPrimary,
-  ),
-);
+//*todo: aqui estamos trabalhando com a serialização dos dados e isso é bom
+//*todo: deixa o código mais rápido.
+//final lightTheme = ThemeData(
+//*todo: para facilitar o hot reload a variável será convertida
+//*todo: para um getter.
+ThemeData get lightTheme => ThemeData(
+      useMaterial3: true,
+      colorScheme: _lightColorScheme,
+      appBarTheme: AppBarTheme(
+        centerTitle: true,
+        backgroundColor: _lightColorScheme.primaryContainer,
+        foregroundColor: _lightColorScheme.onPrimaryContainer,
+      ),
+      floatingActionButtonTheme: FloatingActionButtonThemeData(
+        backgroundColor: _lightColorScheme.primary,
+        foregroundColor: _lightColorScheme.onPrimary,
+      ),
+    );
 
-final darkTheme = ThemeData(
-  useMaterial3: true,
-  colorScheme: _darkColorScheme,
-  appBarTheme: AppBarTheme(
-    centerTitle: true,
-    backgroundColor: _darkColorScheme.primary,
-    foregroundColor: _darkColorScheme.onPrimary,
-  ),
-  floatingActionButtonTheme: FloatingActionButtonThemeData(
-    backgroundColor: _darkColorScheme.primary,
-    foregroundColor: _darkColorScheme.onPrimary,
-  ),
-);
+ThemeData get darkTheme => ThemeData(
+      useMaterial3: true,
+      colorScheme: _darkColorScheme,
+      appBarTheme: AppBarTheme(
+        centerTitle: true,
+        backgroundColor: _darkColorScheme.primary,
+        foregroundColor: _darkColorScheme.onPrimary,
+      ),
+      floatingActionButtonTheme: FloatingActionButtonThemeData(
+        backgroundColor: _darkColorScheme.primary,
+        foregroundColor: _darkColorScheme.onPrimary,
+      ),
+    );
