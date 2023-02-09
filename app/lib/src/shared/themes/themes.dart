da24a14ccacf105a28f163375766e02ee8471f54
MarckVinny <mvrpj@msn.com>
2023-02-08 22:09:34 -0300

    criando o arquivo contendo os temas, themes.dart

---
+++ app/lib/src/shared/themes/themes.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import 'color_schemes.g.dart';
+
+final ligthTheme = ThemeData(
+  useMaterial3: true,
+  colorScheme: lightColorScheme,
+  appBarTheme: AppBarTheme(
+    centerTitle: true,
+    backgroundColor: lightColorScheme.primaryContainer,
+  ),
+  floatingActionButtonTheme: FloatingActionButtonThemeData(
+    backgroundColor: lightColorScheme.primary,
+    foregroundColor: Colors.white,
+  ),
+);
+
+final darkTheme = ThemeData(
+  useMaterial3: true,
+  colorScheme: lightColorScheme,
+  appBarTheme: AppBarTheme(
+    centerTitle: true,
+    backgroundColor: lightColorScheme.primaryContainer,
+  ),
+  floatingActionButtonTheme: FloatingActionButtonThemeData(
+    backgroundColor: lightColorScheme.primary,
+    foregroundColor: Colors.white,
+  ),
+);
