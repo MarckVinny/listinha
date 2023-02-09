737710eb5be1b10085bca740719894370331a8d2
MarckVinny <mvrpj@msn.com>
2023-02-08 21:14:54 -0300

    mudando a cor do tema usando themeMode:

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      themeMode: ThemeMode.light,
       theme: ThemeData(
         useMaterial3: true,
         colorScheme: lightColorScheme,
