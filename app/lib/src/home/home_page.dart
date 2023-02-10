9d699cc806ebfe95b814e6ef93cd36739d31415d
MarckVinny <mvrpj@msn.com>
2023-02-10 02:10:25 -0300

    criando o componente SegmentedButton()

--- app/lib/src/home/home_page.dart
+++ app/lib/src/home/home_page.dart
@@ -11,8 +11,40 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('LISTINHA'),
         actions: const [
-          UserImageButton(),
+          Padding(
+            padding: EdgeInsets.only(right: 8),
+            child: UserImageButton(),
+          ),
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          children: [
+            SegmentedButton(
+              segments: const [
+                ButtonSegment(
+                  value: 0,
+                  label: Text('Todos'),
+                ),
+                ButtonSegment(
+                  value: 1,
+                  label: Text('Pendentes'),
+                ),
+                ButtonSegment(
+                  value: 2,
+                  label: Text('Concluídos'),
+                ),
+                ButtonSegment(
+                  value: 3,
+                  label: Text('Desativados'),
+                ),
+              ],
+              selected: const {0},
+              onSelectionChanged: (values) {},
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton.extended(
         icon: const Icon(Icons.edit),
